00afb1fabf44e8690a5b6e5145077f8ab5f869a8
Susrita Chatterjee <60474018+Susritachatterjee2020@users.noreply.github.com>
2025-09-20 04:56:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 const cards = document.querySelectorAll('.card');
+const highlightCards = document.querySelectorAll('.highlight-item');
+
 window.addEventListener('scroll', () => {
   const triggerBottom = window.innerHeight * 0.85;
+
   cards.forEach(card => {
     const cardTop = card.getBoundingClientRect().top;
-    if (cardTop < triggerBottom) {
-      card.classList.add('visible');
-    }
+    if (cardTop < triggerBottom) card.classList.add('visible');
+  });
+
+  highlightCards.forEach(card => {
+    const cardTop = card.getBoundingClientRect().top;
+    if (cardTop < triggerBottom) card.classList.add('visible');
   });
 });
+
 
 
 
