8c541aa269e518a860f5875c0f83c08db2bd346e
Susrita Chatterjee <60474018+Susritachatterjee2020@users.noreply.github.com>
2025-09-20 05:16:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const cards = document.querySelectorAll('.card');
 const highlightCards = document.querySelectorAll('.highlight-item');
 
-function revealOnScroll() {
+window.addEventListener('scroll', () => {
   const triggerBottom = window.innerHeight * 0.85;
 
   cards.forEach(card => {
@@ -13,13 +13,8 @@ function revealOnScroll() {
     const cardTop = card.getBoundingClientRect().top;
     if (cardTop < triggerBottom) card.classList.add('visible');
   });
-}
+});
 
-// Trigger on scroll
-window.addEventListener('scroll', revealOnScroll);
-
-// Trigger on page load
-window.addEventListener('load', revealOnScroll);
 
 
 
