0856b642cf485632de7560323011831e23d24d74
Susrita Chatterjee <60474018+Susritachatterjee2020@users.noreply.github.com>
2026-03-07 13:21:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+AOS.init({
+duration:1000,
+once:true
+});
+
+particlesJS("particles-js",{
+particles:{
+number:{value:40},
+size:{value:3},
+move:{speed:1},
+line_linked:{enable:true}
+}
+});
+
+
 const toggle=document.querySelector(".menu-toggle")
 const nav=document.querySelector(".nav-links")
 
