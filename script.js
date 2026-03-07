726163ac515568a6ce74b409b92618209bd885e4
Susrita Chatterjee <60474018+Susritachatterjee2020@users.noreply.github.com>
2026-03-07 13:03:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,14 @@
-// NAVBAR TOGGLE
+const toggle=document.querySelector(".menu-toggle")
+const nav=document.querySelector(".nav-links")
 
-const toggle = document.querySelector(".menu-toggle")
-const nav = document.querySelector(".nav-links")
-
-toggle.addEventListener("click", () => {
-
+toggle.addEventListener("click",()=>{
 nav.classList.toggle("active")
-
 })
 
-
-
-// SMOOTH SCROLL
-
-document.querySelectorAll(".nav-links a").forEach(link => {
-
-link.addEventListener("click", function(e){
-
+document.querySelectorAll(".nav-links a").forEach(link=>{
+link.addEventListener("click",function(e){
 e.preventDefault()
-
 const target=document.querySelector(this.getAttribute("href"))
-
-target.scrollIntoView({
-behavior:"smooth"
+target.scrollIntoView({behavior:"smooth"})
 })
-
-})
-
-})
-
-
-
-// EXPERIENCE DROPDOWN
-
-const expButtons=document.querySelectorAll(".exp-btn")
-
-expButtons.forEach(btn => {
-
-btn.addEventListener("click", () => {
-
-const content=btn.nextElementSibling
-
-content.classList.toggle("show")
-
-})
-
 })
